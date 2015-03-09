55ec1ef193d1b34fc5774c03c8673f8b43a0cf64
Yue Du <ifduyue@gmail.com>
2015-03-09 23:33:02 +0800

    Add encrypt_hex and decrypt_hex

--- lib/xxtea.rb
+++ lib/xxtea.rb
@@ -1,2 +1,13 @@
 require 'xxtea/version'
 require 'xxtea/xxtea'
+
+
+module XXTEA
+  def self.encrypt_hex(data, key)
+    encrypt(data, key).unpack('H*').first
+  end
+
+  def self.decrypt_hex(data, key)
+    decrypt([data].pack('H*'), key)
+  end
+end
